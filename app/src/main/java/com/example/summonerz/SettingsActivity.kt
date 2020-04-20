0b08d27e3c7c589bd3d5a457b640c041a6ce1c67
jkursula <tkursula@gmail.com>
2020-04-20 18:59:21 +0300

    CC attribution license

--- app/src/main/java/com/example/summonerz/SettingsActivity.kt
+++ app/src/main/java/com/example/summonerz/SettingsActivity.kt
@@ -2,11 +2,19 @@ package com.example.summonerz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
 
 class SettingsActivity : AppCompatActivity() {
+    private lateinit var creditView:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        creditView = findViewById(R.id.credits_view)
+        creditView.movementMethod = LinkMovementMethod.getInstance()
     }
+
+
 }
